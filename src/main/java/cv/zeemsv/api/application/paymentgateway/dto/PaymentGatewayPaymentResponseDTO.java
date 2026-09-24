7d6d5e3bf7f82dddbf0190ed27c68c1c159efe33
Evandro-Pinto <ivatb05170@gmail.com>
2026-09-24 10:51:02 -0100

    commit endpoints

--- src/main/java/cv/zeemsv/api/application/paymentgateway/dto/PaymentGatewayPaymentResponseDTO.java
+++ src/main/java/cv/zeemsv/api/application/paymentgateway/dto/PaymentGatewayPaymentResponseDTO.java
@@ -1,11 +1,15 @@
 package cv.zeemsv.api.application.paymentgateway.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PaymentGatewayPaymentResponseDTO {
+    @JsonAlias({"intentionId", "intention_id"})
     private String intentionId;
+
+    @JsonAlias({"paymentUrl", "payment_url"})
     private String paymentUrl;
 }
